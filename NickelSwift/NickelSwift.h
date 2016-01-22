9f7b7359f3d844094cfa83ec2df1fd4d64c71d99
riana <r.ralambo@gmail.com>
2016-01-22 21:48:18 +0100

    Initial Commit

---
+++ NickelSwift/NickelSwift.h
@@ -0,0 +1,19 @@
+//
+//  NickelSwift.h
+//  NickelSwift
+//
+//  Created by Riana Ralambomanana on 22/01/2016.
+//  Copyright © 2016 riana.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NickelSwift.
+FOUNDATION_EXPORT double NickelSwiftVersionNumber;
+
+//! Project version string for NickelSwift.
+FOUNDATION_EXPORT const unsigned char NickelSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NickelSwift/PublicHeader.h>
+
+
